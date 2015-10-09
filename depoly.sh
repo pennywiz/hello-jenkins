49f9b9738c869c03da7fa547f76b35692efffdc9
Simone <simone@vangogh-creative.it>
2015-10-09 11:15:06 +0200

    no message

--- depoly.sh
+++ depoly.sh
@@ -1,1 +1,1 @@
-rsync -avzh /Users/Shared/Jenkins/Home/jobs/hello-jenkins/workspace /Users/vangogh/Documents/www/jenkins-base/test-hello
+rsync -avzh /Users/Shared/Jenkins/Home/jobs/hello-jenkins/workspace/ /Users/vangogh/Documents/www/jenkins-base/test-hello
