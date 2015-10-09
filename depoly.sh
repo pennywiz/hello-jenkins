e3db3bacf5d755c8bf76f9167f59c0208c0eb091
Simone <simone@vangogh-creative.it>
2015-10-09 12:02:14 +0200

    no message

--- depoly.sh
+++ depoly.sh
@@ -1,1 +1,6 @@
-sudo rsync -avzh /Users/Shared/Jenkins/Home/jobs/hello-jenkins/workspace/ /Users/vangogh/Documents/www/jenkins-base/test-hello
+#sudo rsync -avzh /Users/Shared/Jenkins/Home/jobs/hello-jenkins/workspace/ /Users/vangogh/Documents/www/jenkins-base/test-hello
+
+rsync -az --progress --rsh='ssh -p8534' --inplace /Users/Shared/Jenkins/Home/jobs/hello-jenkins/workspace/ adminvgg@vangogh-creative.it:/var/www/vhosts/vangogh-creative.it/test.vangogh-creative.it
+#scp -P 8534 .env.dev adminvgg@vangogh-creative.it:/var/www/vhosts/vangogh-creative.it/xbox.vangogh-creative.it/.env
+
+
