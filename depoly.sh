b78a2b7d48c83d1b3ac538a14464e68ea62328e1
Simone <simone@vangogh-creative.it>
2015-10-14 10:09:55 +0200

    no message

--- depoly.sh
+++
@@ -1,3 +0,0 @@
-rsync -az --progress --rsh='ssh -p8534' -e 'ssh -i /.ssh/authorized_keys' --inplace /Users/Shared/Jenkins/Home/jobs/hello-jenkins/workspace/ adminvgg@vangogh-creative.it:/var/www/vhosts/vangogh-creative.it/test.vangogh-creative.it
-
-
