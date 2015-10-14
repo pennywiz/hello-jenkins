9fb749149e946991b9814fd7e54a3ca2f97a0bc2
Simone <simone@vangogh-creative.it>
2015-10-14 11:14:27 +0200

    no message

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,1 @@
 rsync -az --progress --rsh='ssh -p8534' --inplace /Users/Shared/Jenkins/Home/jobs/hello-jenkins/workspace/ root@vangogh-creative.it:/var/www/vhosts/vangogh-creative.it/test.vangogh-creative.it/jenkins
-
-
